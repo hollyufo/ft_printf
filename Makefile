ac593ea030c5654b193db0ec1721dcd102926c18
hollyufo <imranchaibi1@gmail.com>
2024-11-16 16:22:49 +0100

    added needed fucntions

---
+++ Makefile
@@ -0,0 +1,9 @@
+NAME = ft_printf.a
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+
+SRC = ft_printf.c ft_bzero.c ft_memset.c \
+      ft_strcpy.c ft_strlen.c ft_calloc.c \
+      ft_putstr_fd c ft_putchar_fd.c \
+      ft_strcpy
---
+++ ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imchaibi <imchaibi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/16 16:15:15 by imchaibi          #+#    #+#             */
+/*   Updated: 2024/11/16 16:19:29 by imchaibi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <stddef.h>
+# include <stdlib.h> 
+# include <unistd.h>
+# include <stdint.h>
+
+size_t	ft_strlen(const char *str);
+int		ft_printf(const char *format, ...);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+void	*ft_memset(void *s, int c, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+void	ft_bzero(void *s, size_t n);
+
+# endif
