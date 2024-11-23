27f96e360d0c3d27f5f14554a295443fda06deae
hollyufo <imranchaibi1@gmail.com>
2024-11-23 11:22:41 +0100

    done with printf functionality

--- ft_printf.c
+++ ft_printf.c
@@ -11,6 +11,7 @@ void	ft_putstr_fd(char *s, int fd)
 
 	if (!s)
 	{
+        write(fd, "(null)", 6);
 		return ;
 	}
 	i = 0;
@@ -33,6 +34,46 @@ void	ft_putnbr_fd(int nb, int fd)
 		ft_putnbr_fd(num / 10, fd);
 	}
 	ft_putchar_fd((num % 10) + '0', fd);
+}
+void	ft_puthex_fd(unsigned long num, int fd)
+{
+	char	*hex = "0123456789abcdef";
+
+	if (num >= 16)
+		ft_puthex_fd(num / 16, fd);
+	ft_putchar_fd(hex[num % 16], fd);
+}
+void	ft_putptr_fd(void *ptr, int fd)
+{
+	unsigned long	address;
+
+	address = (unsigned long)ptr;
+	ft_putstr_fd("0x", fd);
+	ft_puthex_fd(address, fd); // Pass the cast address
+}
+void	ft_putnbr_unsigned_fd(unsigned int nb, int fd)
+{
+	if (nb >= 10)
+	{
+		ft_putnbr_unsigned_fd(nb / 10, fd);
+	}
+	ft_putchar_fd((nb % 10) + '0', fd);
+}
+void	ft_puthex_lower_fd(unsigned int num, int fd)
+{
+	char	*hex = "0123456789abcdef";
+
+	if (num >= 16)
+		ft_puthex_lower_fd(num / 16, fd);
+	ft_putchar_fd(hex[num % 16], fd);
+}
+void	ft_puthex_upper_fd(unsigned int num, int fd)
+{
+	char	*hex = "0123456789ABCDEF";
+
+	if (num >= 16)
+		ft_puthex_upper_fd(num / 16, fd);
+	ft_putchar_fd(hex[num % 16], fd);
 }
 int	ft_printf(const char *format, ...)
 {
@@ -64,6 +105,26 @@ int	ft_printf(const char *format, ...)
                 double d = va_arg(args, double);
                 printf("%f", d);
             }
+            else if (*format == 'p')
+            {
+                void *ptr = va_arg(args, void *);
+                ft_putptr_fd(ptr, 1);
+            }
+            else if (*format == 'u')
+            {
+                unsigned int num = va_arg(args, double);
+                ft_putnbr_unsigned_fd(num, 1);
+            }
+             else if (*format == 'x')
+            {
+                unsigned int num = va_arg(args, unsigned int);
+                ft_puthex_lower_fd(num, 1);
+            }
+            else if (*format == 'X')
+            {
+                unsigned int num = va_arg(args, unsigned int);
+                ft_puthex_upper_fd(num, 1);
+            }
             else
             {
                 ft_putchar_fd(*format, 1);
@@ -82,7 +143,9 @@ int	ft_printf(const char *format, ...)
 
 int main()
 {
-    ft_printf("ft_printf = Hello %s, %d, %c\n", "world", 42, 'a');
-    // printf("\nprintf = Hello %s, %d, %c, %f, \n", "world", 42, 'a', 3.14);
+    unsigned int num = 3735928559; // Example number (0xDEADBEEF)
+    ft_printf("Lowercase hex: %x\n", num);
+    ft_printf("Uppercase hex: %X\n", num);
+    ft_printf("Another number: %x\n", 255); // Example with small number
     return 0;
 }
