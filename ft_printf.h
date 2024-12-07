f403a97935e8b5a9cbe046ead5c8660648183ceb
hollyufo <imranchaibi1@gmail.com>
2024-12-07 17:46:59 +0100

    removed f and its fucntions

--- ft_printf.h
+++ ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hollyufo <hollyufo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: imchaibi <imchaibi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 16:15:15 by imchaibi          #+#    #+#             */
-/*   Updated: 2024/11/25 22:11:52 by hollyufo         ###   ########.fr       */
+/*   Updated: 2024/12/07 17:46:34 by imchaibi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,6 @@ int		ft_printf(const char *format, ...);
 void	ft_putchar_fd(char c, int fd, int *count);
 void	ft_putstr_fd(char *s, int fd, int *count);
 void	ft_putnbr_fd(int n, int fd, int *count);
-void	ft_putnbr_unsigned_fd(unsigned int n, int fd, int *count);
 void	ft_putptr_fd(void *ptr, int fd, int *count);
 void	ft_puthex(unsigned long num, int fd, char case_type, int *count);
-void	ft_putfloat_fd(double n, int fd, int precision, int *count);
 #endif
